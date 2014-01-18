e2075f3a6815626fad69a60d4492ab979226bcf2
schmidi <schmidi@devlol.org>
2014-01-18 16:06:35 +0100

    added function to build tex for handchange

--- tools/createtext.py
+++ tools/createtext.py
@@ -18,7 +18,34 @@ def buildHomerowTex(filename, homerow, layout):
         color = "red"
       output += "\\textcolor{" + color + "}{" + char + "}"
 
-  newfile = open(filename + "_" + layout + ".tex", 'w+')
+  newfile = open(filename + "_homerow_" + layout + ".tex", 'w+')
+  newfile.write(output)
+
+def get_hand(char, left):
+  if char in left:
+    return 0
+  else:
+    return 1
+
+def buildHandChangeTex(filename, left, layout):
+  file = open(filename, "r")
+  text = file.read()
+  output = ""
+  #0 for left, 1 for right
+  lasthand = -1
+  for char in text:
+    if char == " ":
+      output += char
+    else:
+      hand = get_hand(char, left)
+      if (lasthand == -1) or (lasthand != hand):
+        color = "green"
+      else:
+        color = "red"
+      lasthand = hand
+      output += "\\textcolor{" + color + "}{" + char + "}"
+    
+  newfile = open(filename + "_handchange_" + layout + ".tex", 'w+')
   newfile.write(output)
 
 #main
@@ -26,11 +53,22 @@ if __name__ == "__main__":
   if len(sys.argv) > 1:
     filename = sys.argv[1]
 
-    neo = "uiaeosnrtdyUIAEOSNRTDY\/{}():456"
-    qwertz = "asdfghjklöäASDFGHJKLÖÄ"
+    #changing of hand
+    neo_left = "xvlcwuiaeoüöäp"
+    neo_left += str.upper(neo_left)
 
-    buildHomerowTex(filename, neo, "neo")
-    buildHomerowTex(filename, qwertz, "qwertz")
+    qwertz_left = "qwertasdfgyxcvb"
+    qwertz_left += str.upper(qwertz_left)
+
+    buildHandChangeTex(filename, neo_left, "neo")
+    buildHandChangeTex(filename, qwertz_left, "qwertz")
+
+    # homerow
+    neo_homerow = "uiaeosnrtdyUIAEOSNRTDY\/{}():456"
+    qwertz_homerow = "asdfghjklöäASDFGHJKLÖÄ"
+
+    buildHomerowTex(filename, neo_homerow, "neo")
+    buildHomerowTex(filename, qwertz_homerow, "qwertz")
 
   else:
     print ("missing filename...")
